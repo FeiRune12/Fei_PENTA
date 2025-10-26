d65b0c146979abb0413b96ada65d040f45c9b852
Fei <giovannisilvaalves604@gmail.com>
2025-10-25 21:01:46 -0300

    Remove chamada de teste desnecessária que causava AttributeError

--- run_api.py
+++ run_api.py
@@ -1,7 +1,5 @@
 
 from geb_1_3b import modeling_geblm
 
-modeling_geblm.teste()  # deve imprimir "O módulo está funcionando!"
+# modeling_geblm.teste()  # deve imprimir "O módulo está funcionando!"
 from geb_1_3b import modeling_geblm
-
-modeling_geblm.teste()  # deve imprimir "O módulo está funcionando!"
